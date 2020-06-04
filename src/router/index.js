1a9bd6274c200e99be646a08224e2aa052fe88fd
gl <249278713@qq.com>
2020-06-04 23:05:30 +0800

    frame init

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,90 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ViewUI from 'view-design';
 
+Vue.use(ViewUI);
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+    {
+      path: '/',
+      name: 'Login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      component: () => import('../views/Main.vue'),
+      children: [
+          {
+            path: '/components/ServiceHall',
+            name: 'ServiceHall',
+            component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+          }
+          , {
+            path: '/components/Management',
+            name: 'Management',
+            component: () => import('../views/Nav.vue')
+          },
+           {
+            path: '/components/User',
+            name: 'User',
+            component: () => import('../views/Login.vue')
+          },{
+            path: '/components/Personnel',
+            name: 'Personnel',
+            component: Home
+          },{
+            path: '/components/Alarm',
+            name: 'Alarm',
+            component: Home
+          }
+      ]
+    },
+  ]
 
 const router = new VueRouter({
   routes
 })
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  //to 将要访问的路径
+  //from 代表从那个路径跳转而来
+  //next 是一个函数，放行
+  //    next() 放行     next('/login) 强制跳转
+  ViewUI.LoadingBar.start();
+  //如果用户访问的是登录页面，则直接放行
+  if(to.path === '/'){
+    next()
+  }else{
+    //从 sessionStorage 中获取 保存的 token
+    const user = window.sessionStorage.getItem('user')
+    //如果没有token，强制跳转到登录页面，如果有，则直接放行
+    if(user != 'test'){
+      next('/')
+    }else{
+      next()
+    }
+  }
+});
+
+router.afterEach(route => {
+    ViewUI.LoadingBar.finish();
+});
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loginbody">
+      <el-form v-model="ruleForm"  status-icon  :rules="rules"> 
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input v-model="ruleForm.user" prefix-icon="el-icon-user" placeholder="用户名" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="">
+          <el-input type="password" v-model="ruleForm.passwd" prefix-icon="el-icon-lock" placeholder="密码" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 登录按钮 -->
+        <el-form-item>
+          <el-button type="primary" @click.enter="login()" >登&nbsp;录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() { // 页面的初始数据
+      return {
+        ruleForm: { // 存储用户输入数据
+          user: '',
+          passwd: '',
+        },
+        rules: { // 可添加一些表单规则
+        }
+      }
+    },
+    methods: {
+      login() {
+        if(this.ruleForm.user === 'test' && this.ruleForm.passwd === '123456'){
+          window.sessionStorage.setItem("user", 'test');
+          this.$router.push("/main");
+        }else{
+          alert("账号或密码错误！")
+        }
+      }
+    }
+  }
+</script>
